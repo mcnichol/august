37ec1bd91a1242458199749416918cdbd0f2b846
mcnichol <mcnichol.m@gmail.com>
2016-12-11 16:18:44 -0600

    Initial add of Multiplication Tables java app

---
+++ math/multiplication-tables/src/main/java/com/mcnichol/math/MultiplicationTablesApplication.java
@@ -0,0 +1,12 @@
+package com.mcnichol.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplicationTablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplicationTablesApplication.class, args);
+	}
+}
---
+++ math/multiplication-tables/src/test/java/com/mcnichol/math/MultiplicationTablesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mcnichol.math;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiplicationTablesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
